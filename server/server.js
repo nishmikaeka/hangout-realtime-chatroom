25eec6aedf2705efc6bfeb18cd94152bd65f02f4
nishmikaeka <nurannishmika22@gmail.com>
2025-11-09 18:01:30 +0530

    Path issue fixed

--- server/server.js
+++ server/server.js
@@ -205,8 +205,7 @@ app.use("/api/upload", uploadRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
+app.get(/^\/.*$/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
